09b4e2f889fa0254c121185f7969acca2c93eef5
Nicolas Renvoisé <n.renvoise@ubitransport.com>
2020-02-20 20:51:08 +0100

    refactor: move methods to MapBuilder

--- public/js/src/Map/Map.js
+++ public/js/src/Map/Map.js
@@ -15,50 +15,13 @@ function Map() {
         ) => {
             that.element = document.getElementById(mapElementId);
             that.map = that.builder.buildMap(mapElementId);
-            that.buildMapBoxLayer();
-            that.buildGeoportailLayer('ignMap');
+
+            let ignMap = 'ignMap';
+            let mapBox = 'mapBox';
+
+            that.layers[mapBox] = that.builder.buildMapBoxLayer();
+            that.layers[ignMap] = that.builder.buildGeoportailLayer(ignMap);
             that.builder.addLayersToMap(that.map, that.layers);
-        },
-
-        buildMapBoxLayer: () => {
-            let layerUrl = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}';
-            let options = {
-                attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-                maxZoom: 18,
-                id: 'mapbox.streets',
-                accessToken: 'pk.eyJ1Ijoibmljb3JkZXYiLCJhIjoiY2swbWNrNm1tMTI1MjNtbXpwZ2ZpcXJ6eCJ9.nEonoKrNrSnRZa0RpJz6XA' // My mapbox access token
-            };
-
-            return that.layers.mapBox = that.builder.buildTileLayer(
-                layerUrl,
-                options
-            );
-        },
-
-        buildGeoportailLayer: (layer = 'ignMap') => {
-            let apiKey = "k81gswufyozu38e49nfkcepg";
-            let availableLayers = {
-                ignMap: "GEOGRAPHICALGRIDSYSTEMS.MAPS",
-                ign25000: "GEOGRAPHICALGRIDSYSTEMS.MAPS.SCAN-EXPRESS.STANDARD",
-                ignPhoto: "ORTHOIMAGERY.ORTHOPHOTOS"
-            };
-            let url = "https://wxs.ign.fr/" + apiKey + "/geoportail/wmts?" +
-            "&REQUEST=GetTile&SERVICE=WMTS&VERSION=1.0.0" +
-            "&STYLE=normal" +
-            "&TILEMATRIXSET=PM" +
-            "&FORMAT=image/jpeg" +
-            "&LAYER=" + availableLayers[layer] +
-            "&TILEMATRIX={z}" +
-            "&TILEROW={y}" +
-            "&TILECOL={x}";
-            let options = {
-                minZoom : 0,
-                maxZoom : 18,
-                attribution : "IGN-F/Geoportail",
-                tileSize : 256
-            };
-
-            return that.layers[layer] = that.builder.buildTileLayer(url, options);
         },
 
         /**
